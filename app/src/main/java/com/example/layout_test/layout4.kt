c78096a598bcc7db4333766a68dd0787d0e777f6
Jinwoo <jnu@Jinwooui-MacBookPro.local>
2020-07-07 04:24:20 +0900

    레이아웃 공부했던 자료입니다

---
+++ app/src/main/java/com/example/layout_test/layout4.kt
@@ -0,0 +1,12 @@
+package com.example.layout_test
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class layout4 : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_layout4)
+    }
+}
